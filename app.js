4e4f7380daaf79b3c5c8b36c35be25e9f84abb44
zamieabdullah <zamieabdullah0@gmail.com>
2022-12-07 16:16:44 -0500

    adding a server and dependencies

---
+++ app.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+
+const port = process.env.PORT || 3000
+
+app.use(cors());
+app.use(express.json());
+
+app.listen(port, () => {
+    console.log("Running on port", port);
+});
